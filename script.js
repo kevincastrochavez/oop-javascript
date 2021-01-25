ef1fd65a7b21632698a1a02b2d470611d734a3e1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-25 12:57:43 -0600

    learned about getters ad setters in javascript

--- script.js
+++ script.js
@@ -119,13 +119,13 @@ GOOD LUCK 😀
 // bmw.break();
 // bmw.break();
 
-///////////////////////////////////////
-// ES6 Classes
+// ///////////////////////////////////////
+// // ES6 Classes
 
-// Class expression
-// const PersonCl = class {}
+// // Class expression
+// // const PersonCl = class {}
 
-// Class declaration
+// // Class declaration
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -141,11 +141,32 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.fullName}`);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
 }
 
 const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -157,3 +178,26 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizes
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+// PersonCl.hey();
+
+///////////////////////////////////////
+// Setters and Getters (useful for data validation)
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
