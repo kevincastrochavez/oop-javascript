463d9fb0a92ab85ae1f50dde9bcaa37b9f2c44fe
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-25 18:48:50 -0600

    coding challenge to review inheritance between classes with ES6 classes, getters, setters chaining methods, data privacy and encapsulation

--- script.js
+++ script.js
@@ -451,92 +451,164 @@ GOOD LUCK 😀
 // jay.introduce();
 // jay.calcAge();
 
+// ///////////////////////////////////////
+// // Encapsulation: Protected Properties and Methods
+// // Encapsulation: Private Class Fields and Methods
+
+// // 1) Public fields
+// // 2) Private fields
+// // 3) Public methods
+// // 4) Private methods
+// // (there is also the static version)
+
+// class Account {
+//   // 1) Public fields (same in every instance, not in prototype)
+//   locale = navigator.language;
+
+//   // 2) Private fields (instances)
+//   #movements = [];
+//   #pin;
+
+//   constructor(owner, currency, pin) {
+//     this.owner = owner;
+//     this.currency = currency;
+//     this.#pin = pin;
+
+//     // Protected property
+//     // this._movements = [];
+//     // this.locale = navigator.language;
+
+//     console.log(`Thanks for opening an account, ${owner}`);
+//   }
+
+//   // 3) Public methods
+
+//   // Public interface
+//   getMovements() {
+//     return this.#movements;
+//   }
+
+//   deposit(val) {
+//     this.#movements.push(val);
+//     return this;
+//   }
+
+//   withdraw(val) {
+//     this.deposit(-val);
+//     return this;
+//   }
+
+//   requestLoan(val) {
+//     // if (this.#approveLoan(val)) {
+//     if (this._approveLoan(val)) {
+//       this.deposit(val);
+//       console.log(`Loan approved`);
+//       return this;
+//     }
+//   }
+
+//   static helper() {
+//     console.log('Helper');
+//   }
+
+//   // 4) Private methods
+//   // #approveLoan(val) {
+//   _approveLoan(val) {
+//     return true;
+//   }
+// }
+
+// const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// // acc1._movements.push(250);
+// // acc1._movements.push(-140);
+// // acc1.approveLoan(1000);
+
+// acc1.deposit(250);
+// acc1.withdraw(140);
+// acc1.requestLoan(1000);
+// console.log(acc1.getMovements());
+// console.log(acc1);
+// Account.helper();
+
+// // console.log(acc1.#movements);
+// // console.log(acc1.#pin);
+// // console.log(acc1.#approveLoan(100));
+
+// ///////////////////////////
+// // Chaining
+
+// acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+// console.log(acc1.getMovements());
+
 ///////////////////////////////////////
-// Encapsulation: Protected Properties and Methods
-// Encapsulation: Private Class Fields and Methods
+// Coding Challenge #4
 
-// 1) Public fields
-// 2) Private fields
-// 3) Public methods
-// 4) Private methods
-// (there is also the static version)
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
 
-class Account {
-  // 1) Public fields (same in every instance, not in prototype)
-  locale = navigator.language;
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
 
-  // 2) Private fields (instances)
-  #movements = [];
-  #pin;
+GOOD LUCK 😀
+*/
 
-  constructor(owner, currency, pin) {
-    this.owner = owner;
-    this.currency = currency;
-    this.#pin = pin;
-
-    // Protected property
-    // this._movements = [];
-    // this.locale = navigator.language;
-
-    console.log(`Thanks for opening an account, ${owner}`);
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
   }
 
-  // 3) Public methods
-
-  // Public interface
-  getMovements() {
-    return this.#movements;
-  }
-
-  deposit(val) {
-    this.#movements.push(val);
+  brake() {
+    this.speed -= 20;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
     return this;
   }
 
-  withdraw(val) {
-    this.deposit(-val);
-    return this;
+  set speedUS(currSpeed) {
+    return (this.speed = currSpeed * 1.6);
   }
 
-  requestLoan(val) {
-    // if (this.#approveLoan(val)) {
-    if (this._approveLoan(val)) {
-      this.deposit(val);
-      console.log(`Loan approved`);
-      return this;
-    }
-  }
-
-  static helper() {
-    console.log('Helper');
-  }
-
-  // 4) Private methods
-  // #approveLoan(val) {
-  _approveLoan(val) {
-    return true;
+  get speedUS() {
+    return this.speed / 1.6;
   }
 }
 
-const acc1 = new Account('Jonas', 'EUR', 1111);
+class Electric extends Car {
+  #charge;
 
-// acc1._movements.push(250);
-// acc1._movements.push(-140);
-// acc1.approveLoan(1000);
+  constructor(make, speed, charge) {
+    super(make, speed);
 
-acc1.deposit(250);
-acc1.withdraw(140);
-acc1.requestLoan(1000);
-console.log(acc1.getMovements());
-console.log(acc1);
-Account.helper();
+    this.#charge = charge;
+  }
 
-// console.log(acc1.#movements);
-// console.log(acc1.#pin);
-// console.log(acc1.#approveLoan(100));
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
 
-///////////////////////////
-// Chaining
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} going at ${this.speed}, with a charge of ${this.#charge}%`
+    );
+    return this;
+  }
+}
 
-acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
-console.log(acc1.getMovements());
+const rivian = new Electric('Rivian', 120, 23);
+
+console.log(rivian);
+// console.log(rivian.#charge);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
